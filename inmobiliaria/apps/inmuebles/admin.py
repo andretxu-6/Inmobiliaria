3cddff158bb8be9977034e8067cf082e20a073ff
Andrea Isabel <andretxu6@gmail.com>
2017-06-01 00:15:06 +0200

    prueba

--- inmobiliaria/apps/inmuebles/admin.py
+++ inmobiliaria/apps/inmuebles/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Inmueble
+from .models import Inmueble, alquiler
 
 # Register your models here.
 admin.site.register(Inmueble)
+admin.site.register(alquiler)
