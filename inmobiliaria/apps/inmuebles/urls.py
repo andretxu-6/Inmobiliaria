a332e96f41f04aa5863823b18b973c6ee871641f
andrea <andrea@mennos-Mac-mini.local>
2017-06-02 16:38:16 +0200

    mas info super chachi

--- inmobiliaria/apps/inmuebles/urls.py
+++ inmobiliaria/apps/inmuebles/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
 
-from apps.inmuebles.views import inmuebles, inmueble_view
+from apps.inmuebles.views import inmuebles, inmueble_view, inmueble_edit
 
 urlpatterns = [
 	url(r'^index$', inmuebles, name='inmuebles'),
 	url(r'^nuevo$', inmueble_view, name='inmueble_create'),
+	url(r'^editar/(?P<id_inmueble>\d+)/$', inmueble_edit, name='inmueble_editar'),
 ]
--- inmobiliaria/apps/inmuebles/views.py
+++ inmobiliaria/apps/inmuebles/views.py
@@ -6,7 +6,8 @@ from apps.inmuebles.forms import InmuebleForm
 # Create your views here.
 def inmuebles(request):
 	#return HttpResponse("holadola caracola")
-	inmueble_tot = Inmueble.objects.all()
+	#AQUI PODRIA HACER 2 SELECT, LAS CASAS DISPONIBLES Y LAS NO DISPONIBLES Y MANDAR LAS DOS, CON ESO CREAR LA TABLA EN EL TEMPLATE
+	inmueble_tot = Inmueble.objects.all().order_by('id')
 	enviar = {'inmuebles': inmueble_tot}
 	return render(request, 'inmuebles/index.html',enviar)
 
@@ -20,3 +21,14 @@ def inmueble_view(request):
 		form = InmuebleForm()
 
 	return render(request, 'inmuebles/inmuebles_form.html', {'form': form})
+
+def inmueble_edit(request, id_inmueble):
+	inmueb = Inmueble.objects.get(id=id_inmueble)
+	if request.method == 'GET':
+		form = InmuebleForm(instance=inmueb)
+	else:
+		form = InmuebleForm(request.POST, instance=inmueb)
+		if form.is_valid():
+			form.save()
+		return redirect('inmuebles')
+	return render(request, "inmuebles/inmuebles_form.html", {'form':form})
