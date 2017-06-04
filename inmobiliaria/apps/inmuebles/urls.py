ff5d9eca84232c84466fd23acb808f9a92fb6601
Andrea Isabel <andretxu6@gmail.com>
2017-06-04 13:00:26 +0200

    holadola

--- inmobiliaria/apps/inmuebles/urls.py
+++ inmobiliaria/apps/inmuebles/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
 
-from apps.inmuebles.views import inmuebles, inmueble_view, inmueble_edit
+from apps.inmuebles.views import inmueble_view, inmueble_edit, \
+	Inmuebles, inmueble_delete
 
 urlpatterns = [
-	url(r'^index$', inmuebles, name='inmuebles'),
-	url(r'^nuevo$', inmueble_view, name='inmueble_create'),
-	url(r'^editar/(?P<id_inmueble>\d+)/$', inmueble_edit, name='inmueble_editar'),
+	url(r'^index$', Inmuebles.as_view(), name='inmuebles'),
+	url(r'^nuevo$', inmueble_view.as_view(), name='inmueble_create'),
+	url(r'^editar/(?P<pk>\d+)/$', inmueble_edit.as_view(), name='inmueble_editar'),
+	url(r'^delete/(?P<pk>\d+)/$', inmueble_delete.as_view(), name='inmueble_editar'),
 ]
--- inmobiliaria/apps/inmuebles/views.py
+++ inmobiliaria/apps/inmuebles/views.py
@@ -1,34 +1,59 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 
 from apps.inmuebles.models import Inmueble
 from apps.inmuebles.forms import InmuebleForm
 # Create your views here.
-def inmuebles(request):
-	#return HttpResponse("holadola caracola")
-	#AQUI PODRIA HACER 2 SELECT, LAS CASAS DISPONIBLES Y LAS NO DISPONIBLES Y MANDAR LAS DOS, CON ESO CREAR LA TABLA EN EL TEMPLATE
-	inmueble_tot = Inmueble.objects.all().order_by('id')
-	enviar = {'inmuebles': inmueble_tot}
-	return render(request, 'inmuebles/index.html',enviar)
 
-def inmueble_view(request):
-	if request.method == 'POST':
-		form = InmuebleForm(request.POST)
-		if form.is_valid():
-			form.save()
-		return redirect('inmuebles')
-	else:
-		form = InmuebleForm()
+class Inmuebles(ListView):
+	model = Inmueble
+	template_name = "inmuebles/index.html"
 
-	return render(request, 'inmuebles/inmuebles_form.html', {'form': form})
 
-def inmueble_edit(request, id_inmueble):
-	inmueb = Inmueble.objects.get(id=id_inmueble)
-	if request.method == 'GET':
-		form = InmuebleForm(instance=inmueb)
-	else:
-		form = InmuebleForm(request.POST, instance=inmueb)
-		if form.is_valid():
-			form.save()
-		return redirect('inmuebles')
-	return render(request, "inmuebles/inmuebles_form.html", {'form':form})
+class inmueble_view(CreateView):
+	model = Inmueble
+	form_class = InmuebleForm
+	template_name = 'inmuebles/inmuebles_form.html'
+	success_url = reverse_lazy("inmuebles")
+
+class inmueble_edit(UpdateView):
+	model = Inmueble
+	form_class = InmuebleForm
+	template_name = 'inmuebles/inmuebles_form.html'
+	success_url = reverse_lazy("inmuebles")
+
+class inmueble_delete(DeleteView):
+	model = Inmueble
+	template_name = 'inmuebles/inmuebles_delete.html'
+	success_url = reverse_lazy("inmuebles")
+
+#def inmuebles(request):
+	#inmueble_tot = Inmueble.objects.all().order_by('id')
+	#enviar = {'inmuebles': inmueble_tot}
+	#return render(request, 'inmuebles/index.html',enviar)
+
+
+#def inmueble_view(request):
+#	if request.method == 'POST':
+#		form = InmuebleForm(request.POST)
+#		if form.is_valid():
+#			form.save()
+#		return redirect('inmuebles')
+#	else:
+#		form = InmuebleForm()
+
+#	return render(request, 'inmuebles/inmuebles_form.html', {'form': form})
+
+
+#def inmueble_edit(request, id_inmueble):
+#	inmueb = Inmueble.objects.get(id=id_inmueble)
+#	if request.method == 'GET':
+#		form = InmuebleForm(instance=inmueb)
+#	else:
+#		form = InmuebleForm(request.POST, instance=inmueb)
+#		if form.is_valid():
+#			form.save()
+#		return redirect('inmuebles')
+#	return render(request, "inmuebles/inmuebles_form.html", {'form':form})
